410573e523858d8a1c006d1d486abd34229c255c
JustUser <lskomorohl@gmail.com>
2022-01-20 22:36:07 +0200

    Revert "Added comment"
    This reverts commit 7590d1311f256649a3a27d263ef669fd59e66331.

--- StackUnitTest/stdafx.cpp
+++ StackUnitTest/stdafx.cpp
@@ -6,13 +6,3 @@
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
