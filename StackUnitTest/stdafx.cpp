7590d1311f256649a3a27d263ef669fd59e66331
JustUser <lskomorohl@gmail.com>
2022-01-20 22:21:45 +0200

    Added comment

--- StackUnitTest/stdafx.cpp
+++ StackUnitTest/stdafx.cpp
@@ -6,3 +6,13 @@
 
 // TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
