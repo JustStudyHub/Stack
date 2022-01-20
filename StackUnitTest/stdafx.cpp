310aaea7a5900fb137a0d17d139556727bb4514f
JustUser <lskomorohl@gmail.com>
2022-01-20 22:34:15 +0200

    Revert "modified comment"
    This reverts commit 9948d489ec20faa8720d555dd26a08448605d237.

--- StackUnitTest/stdafx.cpp
+++ StackUnitTest/stdafx.cpp
@@ -16,33 +16,3 @@
 // and not in this file// TODO: reference any additional headers you need in STDAFX.H
 // and not in this file// TODO: reference any additional headers you need in STDAFX.H
 // and not in this file
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file
